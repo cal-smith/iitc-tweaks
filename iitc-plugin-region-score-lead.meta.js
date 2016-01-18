c20fbbbc6e92712c26136c54f25072a1c6545e1f
Callum Smith <hansolo669@users.noreply.github.com>
2016-01-18 01:41:00 -0500

    Update iitc-plugin-region-score-lead.meta.js

--- iitc-plugin-region-score-lead.meta.js
+++ iitc-plugin-region-score-lead.meta.js
@@ -4,8 +4,8 @@
 // @category       Tweaks
 // @version        0.0.1
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      @@UPDATEURL@@
-// @downloadURL    @@DOWNLOADURL@@
+// @updateURL      http://www.reallyawesomedomain.com/iitc-region-score-tweaks/iitc-plugin-region-score-lead.meta.js
+// @downloadURL    http://www.reallyawesomedomain.com/iitc-region-score-tweaks/iitc-plugin-region-score-lead.user.js
 // @description    Small modification to the region scores to show the current mu lead.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*

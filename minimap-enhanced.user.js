4b6808d1a6700d8da42953c35c2bab3187ae64ac
Callum Smith <hansolo669@users.noreply.github.com>
2016-07-24 00:20:39 -0400

    temp url change

--- minimap-enhanced.user.js
+++ minimap-enhanced.user.js
@@ -4,8 +4,8 @@
 // @category       Controls
 // @version        0.1.1.20160724.154202
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      https://secure.jonatkins.com/iitc/release/plugins/minimap.meta.js
-// @downloadURL    https://secure.jonatkins.com/iitc/release/plugins/minimap.user.js
+// @updateURL      https://iitc.reallyawesomedomain.com/minimap-enhanced.user.js
+// @downloadURL    https://iitc.reallyawesomedomain.com/minimap-enhanced.user.js
 // @description    [jonatkins-2015-09-17-154202] Show a mini map on the corner of the map.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*

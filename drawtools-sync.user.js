81bced09d56c7276c3e61412c3aa3799c449746e
Callum Smith <hansolo669@users.noreply.github.com>
2016-10-05 22:33:21 -0400

    Update drawtools-sync.user.js

--- drawtools-sync.user.js
+++ drawtools-sync.user.js
@@ -2,19 +2,19 @@
 // @id             iitc-plugin-drawtools-sync@hansolo669
 // @name           IITC plugin: drawtools sync
 // @category       Tweaks
-// @version        0.1.0
+// @version        0.1.1
 // @namespace      https://github.com/hansolo669/iitc-tweaks
 // @updateURL      https://iitc.reallyawesomedomain.com/drawtools-sync.meta.js
 // @downloadURL    https://iitc.reallyawesomedomain.com/drawtools-sync.user.js
 // @description    Uses the 'sync' plugin to sync drawtools data via the google realtime API.
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
-// @include        https://www.ingress.com/mission/*
-// @include        http://www.ingress.com/mission/*
-// @match          https://www.ingress.com/mission/*
-// @match          http://www.ingress.com/mission/*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
 

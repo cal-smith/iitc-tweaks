047f2703f4d8c1cd4d4852ff48e7e5d9ef0007d7
Callum Smith <hansolo669@users.noreply.github.com>
2016-10-05 22:30:19 -0400

    Fix for intel routing changes

--- chat-tools.meta.js
+++ chat-tools.meta.js
@@ -2,18 +2,18 @@
 // @id             iitc-plugin-chat-tools@hansolo669
 // @name           IITC plugin: chat tools
 // @category       Tweaks
-// @version        0.1.0
+// @version        0.1.1
 // @namespace      https://github.com/hansolo669/iitc-tweaks
 // @updateURL      https://iitc.reallyawesomedomain.com/chat-tools.meta.js
 // @downloadURL    https://iitc.reallyawesomedomain.com/chat-tools.user.js
 // @description    Tools for chat including regex based filters and highligters
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
-// @include        https://www.ingress.com/mission/*
-// @include        http://www.ingress.com/mission/*
-// @match          https://www.ingress.com/mission/*
-// @match          http://www.ingress.com/mission/*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==

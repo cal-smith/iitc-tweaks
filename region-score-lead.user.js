1c14360136c51857524e3f8fef048215e0e8824c
Callum Smith <hansolo669@users.noreply.github.com>
2016-10-05 22:34:55 -0400

    Fix for intel routing changes

--- region-score-lead.user.js
+++ region-score-lead.user.js
@@ -2,19 +2,19 @@
 // @id             iitc-plugin-region-score-lead@hansolo669
 // @name           IITC plugin: region score lead
 // @category       Tweaks
-// @version        0.2.3
+// @version        0.2.4
 // @namespace      https://github.com/hansolo669/iitc-tweaks
 // @updateURL      https://iitc.reallyawesomedomain.com/region-score-lead.meta.js
 // @downloadURL    https://iitc.reallyawesomedomain.com/region-score-lead.user.js
 // @description    Small modification to the region scores to show the current mu lead.
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
-// @include        https://www.ingress.com/mission/*
-// @include        http://www.ingress.com/mission/*
-// @match          https://www.ingress.com/mission/*
-// @match          http://www.ingress.com/mission/*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
 

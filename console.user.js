f02bfe37920cfb1bcee832345953ee5f71875186
Callum Smith <hansolo669@users.noreply.github.com>
2016-10-05 22:32:30 -0400

    Fix for intel routing changes

--- console.user.js
+++ console.user.js
@@ -7,14 +7,14 @@
 // @updateURL      https://iitc.reallyawesomedomain.com/console.meta.js
 // @downloadURL    https://iitc.reallyawesomedomain.com/console.user.js
 // @description    Utility to pipe the standard console back into IITC and easily eval snippets
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
-// @include        https://www.ingress.com/mission/*
-// @include        http://www.ingress.com/mission/*
-// @match          https://www.ingress.com/mission/*
-// @match          http://www.ingress.com/mission/*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
 
